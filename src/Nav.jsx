90d4a04fee91e6c91713bc499e9bdbd2402099e1
yoshiodei <yoshiodei@yahoo.com>
2021-07-28 02:10:32 +0000

    final commit

---
+++ src/Nav.jsx
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom';
+
+const Nav = () => {
+    return(
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+        </div>
+    );
+}
+
+export default Nav;
---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return(
+        <div>
+            <h2>Contact</h2>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel dolorum quia, suscipit aut alias inventore saepe, eveniet cum et odio in magnam tenetur beatae ea assumenda recusandae nobis voluptas quis impedit itaque animi omnis sit cupiditate? Earum porro hic quia, placeat, a at maiores. Quasi autem repellat reiciendis, ducimus atque!</div>
+        </div>
+    );
+}
+
+export default Contact; 
